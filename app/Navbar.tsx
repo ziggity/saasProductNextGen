4b752d5ab28a9ea9569ebcef413411687603c264
zachbeecher <zachbeecher@gmail.com>
2024-04-08 11:50:24 -0700

    kinde auth signup basic setup

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,32 @@
 import Link from "next/link";
 import { ThemeToggle } from "./components/Themetoggle";
+import { Button } from "@/components/ui/button"
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export function Navbar() {
-    return (
-        <nav className="border-b bg-background h-[10vh] flex items-center">
-            <div className="container flex items-center justify-between">
-                <Link href="/">
-                    <h1 className="font-bold text-3xl">Nextgen Saas</h1>
-                </Link>
-                <div className="flex items-center gap-x-5">
-                    <ThemeToggle></ThemeToggle>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-3xl">Nextgen Saas</h1>
+        </Link>
+
+        <div className="flex items-center gap-x-5">
+          <ThemeToggle/>
+        
+        <div className="flex items-center gap-x-5">
+        <LoginLink>
+            <Button>Sign in</Button>
+          </LoginLink>
+          <RegisterLink>
+            <Button variant="secondary">Sign up</Button>
+          </RegisterLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage(){
+    return (
+        <div>
+            <h1>Hello from dashboard</h1>
+        </div>
+    )
+}
